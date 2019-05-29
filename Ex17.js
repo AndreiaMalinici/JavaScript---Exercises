6517f92f53307bdfa22374009940540f3fa494a1
AndreiaMalinici <48523662+AndreiaMalinici@users.noreply.github.com>
2019-05-29 11:33:21 +0100

    Add files via upload

---
+++ Ex17.js
@@ -0,0 +1,30 @@
+function MouseOver(MyImage){
+
+    MyImage.src = "./images/image2.jpg";
+}
+
+function MouseOut(MyImage){
+
+	MyImage.src = "./images/image3.jpg";
+}
+
+function bigImg(SecondImage){
+
+	SecondImage.style.height = "250px";
+	SecondImage.style.width = "350px";
+}
+
+function normalImg(SecondImage){
+	SecondImage.style.height = "100px";
+	SecondImage.style.width = "200px";
+}
+
+function smallImg(ThirdImage){
+	ThirdImage.style.height = "100px";
+	ThirdImage.style.width = "200px";
+}
+
+function norImg(ThirdImage){
+	ThirdImage.style.height = "250px";
+	ThirdImage.style.width = "350px";
+}
